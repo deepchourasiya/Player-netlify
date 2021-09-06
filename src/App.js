c28710086794cb604809a0b63267bc05103addc3
Akash Deep <deep.akash@mahindra.com>
2021-09-06 11:40:23 +0530

    Ini project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=mOUU8IEFoWo"
+        controls
+        // width="100%"
+        // height="100%"
+        playing
+        onReady={() => console.log("Player is ready")}
+        onError={(err) => console.log("Error", err)}
+      />
     </div>
   );
 }
